b0f90647fe8f9ca013277b935590bb7f65172de0
Dany Jupille <dany.jupille@he-arc.ch>
2014-05-27 07:00:17 +0200

    Deleted old config files
    - With suffix "Config"
    - With suffix "Configuration"

--- src/main/java/ch/hearc/gotit/configurations/ServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ch.hearc.gotit.configurations;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "ch.hearc.gotit.services.impl")
-public class ServiceConfiguration {
-}
